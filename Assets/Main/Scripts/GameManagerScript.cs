e3e7199d60182797cad6d4feb5c6d6f940474fab
landry42 <landry.valetoux@gmail.com>
2024-01-28 00:11:40 +0100

    Added MainScript

---
+++ Assets/Main/Scripts/GameManagerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // When the game is over, this method is called
+    void OnGameOver()
+    {
+
+    }
+    
+    // When it is on the next page
+    void OnNextPage()
+    {
+
+    }
+}
