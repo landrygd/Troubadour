500b355e3691506e60d99d3c82cd1b4c504c1f31
Mr-Shadow-Mc <82217376+Mr-Shadow-Mc@users.noreply.github.com>
2024-01-27 10:56:24 +0100

    Update Khaled Part [Controlleur]

---
+++ Assets/Khaled/Scripts/InputsReader.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputsReader : MonoBehaviour
+{
+    public Troubadour _inputs;
+
+    public bool _rightHand, _rightLeg, _leftHand, _leftLeg;
+
+    private void Awake()
+    {
+        _inputs = new Troubadour();
+
+        /// Start
+        _inputs.Player.Right_hand.started += RightHand;
+        _inputs.Player.Right_legs.started += RightLeg;
+        _inputs.Player.Left_hand.started += LeftHand;
+        _inputs.Player.Left_legs.started += LeftLeg;
+
+        /// End
+        _inputs.Player.Right_hand.canceled += RightHand;
+        _inputs.Player.Right_legs.canceled += RightLeg;
+        _inputs.Player.Left_hand.canceled += LeftHand;
+        _inputs.Player.Left_legs.canceled += LeftLeg;
+    }
+
+
+    private void OnEnable()
+    {
+        _inputs.Enable();
+    }
+
+
+    private void OnDisable()
+    {
+        _inputs.Disable();
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    ///     FUNCTIONS 
+    
+    public void RightHand(InputAction.CallbackContext context)
+    {
+        _rightHand = context.ReadValueAsButton();
+    }
+
+    public void RightLeg(InputAction.CallbackContext context)
+    {
+        _rightLeg = context.ReadValueAsButton();
+    }
+
+    public void LeftHand(InputAction.CallbackContext context)
+    {
+        _leftHand = context.ReadValueAsButton();
+    }
+
+    public void LeftLeg(InputAction.CallbackContext context)
+    {
+        _leftLeg = context.ReadValueAsButton();
+    }
+}
